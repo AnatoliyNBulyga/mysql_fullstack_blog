526e5d8e65d35c27ef98caec602bcfafd53912de
Anatoliy <anatoliynbulyga@gmail.com>
2022-11-06 14:51:31 +0200

    deleted old api

--- api/db.js
+++
@@ -1,9 +0,0 @@
-import mysql from 'mysql2';
-
-export const db = mysql.createPool({
-    connectionLimit: 10,
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "fullstack_blog",
-});
